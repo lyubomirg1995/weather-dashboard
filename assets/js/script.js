ff5f798b8e9a82129051b46e3b3e9014e976391b
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-24 15:33:17 -0400

    jQuery assigned all relevant variables including APIKeya and queryURL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-//define variables
+var APIKey = "68a6c3ea978b84aa2cc19e3a7396855c"; 
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var dateEl = $('.card-title');
+var tempEl = $('#temperature');
+var humEl = $('#humidity');
+var windEl = $('#wind-speed');
+var cityInput = $('#city-input');
+var cityForm = $('#search-cities');
+var dataOutput = $('#weather-card');
+var clearHistory = $('#clear-history');
+var historyContainer = $('#search-history-container');
+var futureEl = $('#future-forecast');
+
