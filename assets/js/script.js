26f56ca3727da9a1794812f6f68a10cfe4834fec
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-25 08:36:38 -0400

    fixed undefined city variable error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var APIKey = "68a6c3ea978b84aa2cc19e3a7396855c";
+var city = $('#city-input').val()
 var queryURL =
   "http://api.openweathermap.org/data/2.5/weather?q=" +
   city +
@@ -17,7 +18,7 @@ var futureEl = $("#future-forecast");
  
 
 function getWeatherData() {
-  
+   city = $('#city-input').val()
   fetch(queryURL).then(function (response) {
     console.log(response);
     if (response.ok) {
