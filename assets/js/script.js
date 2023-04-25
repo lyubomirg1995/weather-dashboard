585a2f5880aa58627afa0147ec04268e6bd997cc
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-25 07:46:51 -0400

    added fetch API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
-var APIKey = "68a6c3ea978b84aa2cc19e3a7396855c"; 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var dateEl = $('.card-title');
-var tempEl = $('#temperature');
-var humEl = $('#humidity');
-var windEl = $('#wind-speed');
-var cityInput = $('#city-input');
-var cityForm = $('#search-cities');
-var dataOutput = $('#weather-card');
-var clearHistory = $('#clear-history');
-var historyContainer = $('#search-history-container');
-var futureEl = $('#future-forecast');
+var APIKey = "68a6c3ea978b84aa2cc19e3a7396855c";
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+var dateEl = $(".card-title");
+var tempEl = $("#temperature");
+var humEl = $("#humidity");
+var windEl = $("#wind-speed");
+var cityInput = $("#city-input");
+var cityForm = $("#search-cities");
+var dataOutput = $("#weather-card");
+var clearHistory = $("#clear-history");
+var historyContainer = $("#search-history-container");
+var futureEl = $("#future-forecast");
+ 
 
+function getWeatherData() {
+  
+  fetch(queryURL).then(function (response) {
+    console.log(response);
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+}
