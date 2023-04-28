dc49943afe6971837f426f6188bc8a80b87f8780
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-27 22:00:38 -0400

    converted cards to row formation

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# weather-dashboard
+# Open Weather Dashboard
+
+## Description
+Planning a vacation? Need to know what the weather is like several days in advance before making your decision to travel? Introducing Open Weather Dashboard. Based on the Open Weather API, the Open Weather Dashboard application will give you the current weather data, as well as a 5-day weather forecast, for cities in the United States. 
+
+Most weather applications contain complicated user-interfaces, with bloated featureware. While the average person is intimately familiar with consumer technology, some elderly populations around the world, who did not grow up with computers, nor wished to assimilate with a technological society, may experience information overload or frustration navigating modern websites.  With this application, getting the information you need is one click away, without the distraction of many surrounding features/UIs. Building this application reinforces jQuery concepts, for loops, event delegation, localStorage, conditional statements, fetch API, and server-side API techniques. 
+
+***(Under Construction)***
+
+## Usage
+
+1. Click the text-input field.
+2. Type a city name in the United States.
+3. Click Search
+4. Click the Clear button, if you wish to clear your search history. 
+
+## Credits
+
+HTML structure, lines 31-76; JS variable assignment, lines 1-14: https://www.youtube.com/watch?v=bCymBWY1UC0&t=147s;
+
+Current + 5-day forecast console data, lines 17-68: AskBC Learning Assistants: seanbbyfield and talexander (direct code solutions);
+
+for loop setup, line 75: GTP-4 tutor prompt (NO direct code solution);
+
+localStorage debugging, lines 137-160, 168-179, 181: GTP-4 (direct code solution);
+
+Display current + future weather data from search history, lines 188-191: GTP-4 tutor prompt (NO direct code solution);
+
+Clear history, lines 193-197: GTP-4 (direct code solution);
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//assign variables with jQuery selection of all relevant HTML elements
 var APIKey = "68a6c3ea978b84aa2cc19e3a7396855c";
 var city = $("#city-input").val();
 var dateEl = $(".card-title");
@@ -14,7 +15,7 @@ var searchButton = $("#search-city");
 var today = dayjs().format("M-D-YYYY");
 
 function getWeatherData(event) {
-  event.preventDefault();
+  event.preventDefault(); //ensures city input is processed by preventing page refresh
   city = $("#city-input").val();
 
   getCurrentWeatherData(city);
@@ -23,6 +24,7 @@ function getWeatherData(event) {
 function getCurrentWeatherData(city) {
   //console commands provide essential data collection for further feature implementation
   console.log("City ", city);
+  
   var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     city +
@@ -30,10 +32,11 @@ function getCurrentWeatherData(city) {
     "&appid=" +
     APIKey;
   console.log("Query ", queryURL);
-
+  
+  //output current weather data
   fetch(queryURL)
     .then((response) => response.json())
-    .then((result) => {
+    .then((result) => { // result object is logged to the console 
       console.log("Data ", result);
       //converts unix dt to human readable time
       //unix and date variables moved out of global scope to 2nd then block to avoid undefined result variable
@@ -46,7 +49,7 @@ function getCurrentWeatherData(city) {
       var iconDisplay = $('#wicon').attr('src', iconUrl);
 
       getFiveDayForecastData(lat, lon);
-
+//imperial units needed for Fahrenheit value, lat and lon needed for 5-day forecast
       function getFiveDayForecastData(lat, lon) {
         var fiveDayApiUrl =
           "http://api.openweathermap.org/data/2.5/forecast?" +
@@ -68,16 +71,18 @@ function getCurrentWeatherData(city) {
 
             futureEl.empty(); // prevents new forecast data from appending to old forecast data
 
+            //increments every 7, instead of 8, to ensure 5 data points from an array of 40 objects
             for (var i = 8; i < resultFuture.list.length; i += 7) {
               var arrayData = resultFuture.list[i];
               fiveDayForecast.push(arrayData);
-
+           
               var futureUnixTimeStamp = arrayData.dt;
+              //converts unix time to human time
               var futureDate = new Date(futureUnixTimeStamp * 1000);
               var futureReadableData = futureDate.toLocaleDateString("en-US");
-
+              //dynamically generating card template to be created 5 times for each successive day
               var futureCardEl = $("<div></div>").attr("id", "future-card");
-              futureCardEl.addClass("card");
+              futureCardEl.addClass("card me-4");
               futureEl.append(futureCardEl);
               
 
@@ -89,6 +94,7 @@ function getCurrentWeatherData(city) {
               var futureIconDisplay = $("<img>").attr('src', futureIconUrl);
 
               var futureCardTitleEl = $("<h5></h5>").addClass("card-title");
+              //concatenating an empty space, followed by appending icon to date text ensures its positioning next to the date
               futureCardTitleEl.text(futureReadableData + " ");
               futureCardTitleEl.append(futureIconDisplay);
               futureCardBodyEl.append(futureCardTitleEl);
@@ -96,7 +102,7 @@ function getCurrentWeatherData(city) {
               
 
 
-
+              //each content component on one line is created with successive concatenations between jQuery-created <p> tags
               var futureTempEl = $(
                 "<p>" + "Temperature: " + arrayData.main.temp + "°F" + "</p>"
               );
@@ -119,7 +125,7 @@ function getCurrentWeatherData(city) {
             return resultFuture;
           });
       }
-
+      //dynamically generating current weather card with jQuery, with same logic for creating content on one line, as commented above
       dataOutput.css("display", "block");
       dateEl.text(city + ", " + date.toLocaleDateString("en-US") + " ");
       dateEl.append(iconDisplay);
@@ -127,9 +133,11 @@ function getCurrentWeatherData(city) {
       windEl.text("Wind: " + result.wind.speed + " MPH");
       humEl.text("Humidity: " + result.main.humidity + "%");
 
+      //tries to get 'searchHistory' item from localStorage and parse it into an array. If it doesn't exist or is null, the OR operator ensures searchHistoryArray is assigned an array.
       var searchHistoryArray =
         JSON.parse(localStorage.getItem("searchHistory")) || [];
 
+      //conditional prevents a duplicate from being added to search history
       if (!searchHistoryArray.includes(city)) {
         searchHistoryArray.push(city);
         localStorage.setItem(
@@ -156,7 +164,7 @@ $("#search-city").on("click", getWeatherData);
 function retrieveStoredHistory() {
   var searchHistoryArray =
     JSON.parse(localStorage.getItem("searchHistory")) || [];
-
+//recreates previous searches with jQuery-created buttons, then appends them to search-history-container
   for (var i = 0; i < searchHistoryArray.length; i++) {
     var searchHistoryList = $("<li></li>");
     searchHistoryList.css("list-style", "none");
@@ -170,18 +178,19 @@ function retrieveStoredHistory() {
   }
 }
 
-retrieveStoredHistory();
+retrieveStoredHistory(); // ensures search history is loaded as the page loads or refreshes
 
 $("#search-history-container").on("click", function (event) {
   event.preventDefault();
   var clickedButton = event.target;
 
+//current and future weather data is populates the screen after clicking a button in the search history
   if ($(clickedButton).hasClass("search-history-btn")) {
     city = $(clickedButton).data("city");
     getCurrentWeatherData(city);
   }
 });
-
+//clears search history by clearing local storage
 $("#clear-history").on("click", function () {
   localStorage.removeItem("searchHistory");
   $("#search-history-container").empty();
